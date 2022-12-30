6f96d6cb00edb38bd9c995d980fbe9b978774838
a2nr <anggoro.dwi.nr@gmail.com>
2022-12-30 07:52:12 +0700

    modbus berhasil berjalan

--- mytest/modbus_test/main.cpp
+++ mytest/modbus_test/main.cpp
@@ -1,19 +1,164 @@
+#include <cstdint>
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <unistd.h>
 #include <modbus/modbus.h>
+
 #include <device.h>
 #include <freqGenerator.h>
 
-int main(int argc, char *argv[])
+#define  WORKS_IP       "0.0.0.0"
+#define  WORKS_PORT     1502
+
+volatile bool runIt = true;
+
+modbus_mapping_t *registerMap;
+
+uint16_t bitCoilMaxAlocation;
+uint16_t bitIntpMaxAlocation;
+
+
+void stopIt(int)
 {
-    modbus_t *mb;
+        runIt = false;
+        printf("someone press CTRL+c, program stop");
+}
 
-    mb = modbus_new_tcp("127.0.0.1",1502);
-    modbus_connect(mb);
+int main(void)
+{
+        modbus_t *Context;
+        int masterSocket = -1;
+        int rc;
+        fd_set refSDesc;
+        fd_set setSDesc;
+        int maxFDesc;
+        int curenMSocket;
+        struct timeval timeout;
+        timeout.tv_sec = 3;
 
-    printf("HELLOO COK, WE READY TO MODCOK!! \r");
+        signal(SIGINT, stopIt);
 
-    modbus_close(mb);
-    modbus_free(mb);
+        Context = modbus_new_tcp(WORKS_IP,WORKS_PORT);
+
+        bitCoilMaxAlocation = 0;
+        bitIntpMaxAlocation = 0;
+
+        init_device();
+
+        registerMap = modbus_mapping_new(
+                    bitCoilMaxAlocation /*bit*/,
+                    bitIntpMaxAlocation /*in bit*/,
+                    0 /*h register*/, 0 /*in register*/);
+
+        registerMap->tab_bits = (uint8_t*) malloc(sizeof(uint8_t)*bitCoilMaxAlocation);
+
+
+        modbus_set_debug(Context, TRUE);
+
+        masterSocket = modbus_tcp_listen(Context, 1);
+
+        if(registerMap == NULL || Context == NULL)
+        {
+                printf("[SYS]\t\t Failed to init %s\r", modbus_strerror(errno));
+
+                modbus_free(Context);
+                return -1;
+        }
+
+        printf("[SYS]\t\t HELLOO COK, WE READY TO MODCOK!! \r\n");
+        printf("[SYS]\t\t IP : %s/%d \r\n", WORKS_IP, WORKS_PORT);
+
+        FD_ZERO(&refSDesc);
+        FD_SET(masterSocket,&refSDesc);
+
+        maxFDesc = masterSocket;
+
+
+        while(runIt)
+        {
+                usleep(500);
+                setSDesc = refSDesc;
+                if(select(maxFDesc+1, &setSDesc, NULL, NULL, &timeout) == -1)
+                {
+                        printf("[SYS]\t\t select() failure.");
+                        continue;
+                }
+
+                for(curenMSocket = 0; curenMSocket <= maxFDesc; curenMSocket++)
+                {
+                        if(!FD_ISSET(curenMSocket, &setSDesc))
+                                continue;
+
+                        if(curenMSocket != masterSocket)
+                        {
+                                uint8_t aduBuffer[MODBUS_TCP_MAX_ADU_LENGTH];
+                                int data = 0;
+                                int address = 0;
+
+
+                                modbus_set_socket(Context, curenMSocket);
+                                rc = modbus_receive(Context, aduBuffer);
+
+                                if(rc != -1)
+                                {
+                                        modbus_reply(Context, aduBuffer, rc, registerMap);
+                                        start_device();
+
+                                        continue;
+                                }
+                                printf("[SYS]\t\t Connection closed on socket %d\n", curenMSocket);
+                                close(curenMSocket);
+
+                                FD_CLR(curenMSocket, &refSDesc);
+
+                                if(curenMSocket == maxFDesc)
+                                        maxFDesc--;
+
+                                continue;
+
+                        }
+
+                        socklen_t addrlen;
+                        struct sockaddr_in clientaddr;
+                        int newSDesc;
+
+                        addrlen = sizeof(clientaddr);
+                        memset(&clientaddr, 0, sizeof(clientaddr));
+                        newSDesc = accept(masterSocket,(struct sockaddr *)&clientaddr, &addrlen);
+
+                        if(newSDesc == -1)
+                        {
+                                printf("[SYS]\t\t accept() error");
+                                continue;
+                        }
+
+                        FD_SET(newSDesc, &refSDesc);
+
+                        if(newSDesc > maxFDesc)
+                                maxFDesc = newSDesc;
+
+                        printf("[SYS]\t\t new Connection ok!\r\n" );
+
+                }
+        }
+
+        printf("[SYS]\t\t Quit the loop: %s \r", modbus_strerror(errno));
+
+        if(masterSocket != -1)
+        {
+                close(masterSocket);
+        }
+
+        modbus_mapping_free(registerMap);
+
+        modbus_close(Context);
+        modbus_free(Context);
 }
 
 void initPin(unsigned char, unsigned char)
@@ -24,7 +169,7 @@ void setPin(unsigned char, unsigned char)
 }
 bool getPin(unsigned char)
 {
-    return false;
+        return false;
 }
 void freqGenerator_setPeriod(unsigned char, unsigned long)
 {
@@ -35,5 +180,5 @@ void freqGenerator_setOut(unsigned char, bool)
 }
 unsigned long plclib_millis()
 {
-        return 0;
+        return clock();
 }
